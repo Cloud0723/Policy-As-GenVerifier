48aeed2438c436151be721a804d3ee9bfef3a359
Chenliang Li <chenliang@example.com>
2025-12-17 10:19:19 -0600

    add setupbash

---
+++ quick_start/H100_pag_turn8.sh
@@ -0,0 +1,85 @@
+set -x
+
+export WANDB_API_KEY="810f91e58aa0fd1d03b11c60b0d1cffbb1d941f4"
+export WANDB_ENTITY="rl_agent"
+
+WAND_PROJECT='Policy-As-GenVerifier'
+
+math500=datasets/math500.parquet
+math7500=datasets/math7500.parquet
+aime2024=datasets/aime2024.parquet
+aime2025=datasets/aime2025.parquet
+minervamath=datasets/minervamath.parquet
+dapo17k=datasets/dapo17k.parquet
+
+PROJECT_NAME='PAG'
+CKPT_PATH=checkpoints
+MODEL_PATH=Qwen/Qwen2.5-1.5B-Instruct
+
+EXPERIMENT_NAME="qwen1p5b_pag_8turns"
+n=4
+rollout_type=pag
+num_turns=8
+policy_rs=True
+rs_coef=1.0
+norm_type=role
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=gae \
+    data.train_files=[$math7500] \
+    data.val_files="['$math500']" \
+    data.filter_overlong_prompts=True \
+    data.train_batch_size=512 \
+    data.max_prompt_length=1024 \
+    data.max_response_length=2048 \
+    actor_rollout_ref.model.path=$MODEL_PATH \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.use_dynamic_bsz=True \
+    actor_rollout_ref.actor.ppo_max_token_len_per_gpu=32768 \
+    actor_rollout_ref.rollout.max_num_batched_tokens=32768 \
+    actor_rollout_ref.rollout.enforce_eager=False \
+    actor_rollout_ref.rollout.free_cache_engine=False \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.optim.lr_warmup_steps_ratio=0 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=128 \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.actor.use_kl_loss=False \
+    actor_rollout_ref.actor.entropy_coeff=0 \
+    actor_rollout_ref.actor.clip_ratio_high=0.28 \
+    actor_rollout_ref.actor.clip_ratio_low=0.2 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
+    actor_rollout_ref.rollout.n=$n \
+    actor_rollout_ref.rollout.top_k=10000 \
+    actor_rollout_ref.rollout.num_turns=$num_turns \
+    actor_rollout_ref.rollout.rollout_type=$rollout_type \
+    actor_rollout_ref.rollout.val_kwargs.n=8 \
+    actor_rollout_ref.rollout.val_kwargs.do_sample=True \
+    actor_rollout_ref.rollout.val_kwargs.top_k=-1 \
+    actor_rollout_ref.rollout.val_kwargs.top_p=0.95 \
+    actor_rollout_ref.rollout.val_kwargs.temperature=0.6 \
+    actor_rollout_ref.rollout.val_kwargs.num_turns=$num_turns \
+    reward_model.policy_rs=$policy_rs \
+    reward_model.rs_coef=$rs_coef \
+    critic.optim.lr=2e-6 \
+    critic.use_dynamic_bsz=True \
+    critic.model.use_remove_padding=True \
+    critic.model.path=$MODEL_PATH \
+    critic.model.fsdp_config.param_offload=False \
+    critic.model.fsdp_config.optimizer_offload=False \
+    algorithm.use_kl_in_reward=False \
+    algorithm.norm_type=$norm_type \
+    trainer.logger=['console','wandb'] \
+    trainer.project_name=$PROJECT_NAME \
+    trainer.experiment_name=$EXPERIMENT_NAME \
+    trainer.n_gpus_per_node=8 \
+    trainer.nnodes=1 \
+    trainer.save_freq=-1 \
+    trainer.test_freq=10 \
+    trainer.total_epochs=40 \
+    trainer.default_local_dir=$CKPT_PATH/$PROJECT_NAME/$EXPERIMENT_NAME \
+    trainer.val_before_train=True \
+    trainer.resume_mode=auto \
+    trainer.log_val_generations=2
--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 # 1. Conda 环境配置
 # conda init
-# conda create -n verl030p1-env python=3.10 -y
-# conda activate verl030p1-env
 
 # 2. 安装 PyTorch, Flash Attention, vLLM
 
@@ -10,13 +8,20 @@
 # 2. vLLM 版本已更新为 0.8.2
 # 3. Flash Attention 版本保持不变
 # pip install torch==2.4.0 --index-url https://download.pytorch.org/whl/cu121
+
+conda create -n pag_test python=3.10 -y
+conda activate pag_test
+
 pip install flash-attn==2.7.4.post1 --no-build-isolation
 pip install vllm==0.8.2
 
 pip install verl==0.3.0.post1
 
+pip uninstall verl -y
 pip install flash-attn==2.7.4.post1 --no-build-isolation
 pip install vllm==0.8.2
 
 # 4. 安装其他依赖
 pip install wandb
+pip install ray==2.48.0
+pip install math_verify
